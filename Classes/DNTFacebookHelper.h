0784ce6a1266c505a43018906652997fecc750a8
Daniel Thorpe <dan@blindingskies.com>
2013-04-28 16:10:29 +0100

    Adds empty Facebook helper class.
    Signed-off-by: Daniel Thorpe <dan@blindingskies.com>

---
+++ Classes/DNTFacebookHelper.h
@@ -0,0 +1,13 @@
+//
+//  DNTFacebookHelper.h
+//  You Owe Me Too
+//
+//  Created by Daniel Thorpe on 21/04/2013.
+//  Copyright (c) 2013 Blinding Skies. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DNTFacebookHelper : NSObject
+
+@end
