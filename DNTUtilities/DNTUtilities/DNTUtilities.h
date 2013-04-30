08a7700dc39d57b2cb160071a7a4620a591fed02
Daniel Thorpe <dan@blindingskies.com>
2013-04-30 23:07:40 +0100

    Updates the paths and build settings.
    Signed-off-by: Daniel Thorpe <dan@blindingskies.com>

---
+++ DNTUtilities/DNTUtilities/DNTUtilities.h
@@ -0,0 +1,17 @@
+//
+//  DNTUtilities.h
+//  DNTUtilities
+//
+//  Created by Daniel Thorpe on 30/04/2013.
+//  Copyright (c) 2013 Daniel Thorpe. All rights reserved.
+//
+
+#ifndef DNTUtilities_DNTUtilities_h
+#define DNTUtilities_DNTUtilities_h
+
+// Categories
+#import "UIColor+DNT.h"
+#import "UIView+DNT.h"
+#import "CALayer+DNT.h"
+
+#endif
