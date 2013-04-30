d48fc667a394ce30cbfa9c9735b7014faef88db7
Daniel Thorpe <dan@blindingskies.com>
2013-04-30 21:24:53 +0100

    Adds a DNTUtilities library Xcode project.
    Signed-off-by: Daniel Thorpe <dan@blindingskies.com>

---
+++ DNTUtilities/DNTUtilitiesTests/DNTUtilitiesTests.h
@@ -0,0 +1,13 @@
+//
+//  DNTUtilitiesTests.h
+//  DNTUtilitiesTests
+//
+//  Created by Daniel Thorpe on 30/04/2013.
+//  Copyright (c) 2013 Daniel Thorpe. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface DNTUtilitiesTests : SenTestCase
+
+@end
---
+++ DNTUtilities/DNTUtilitiesTests/DNTUtilitiesTests.m
@@ -0,0 +1,32 @@
+//
+//  DNTUtilitiesTests.m
+//  DNTUtilitiesTests
+//
+//  Created by Daniel Thorpe on 30/04/2013.
+//  Copyright (c) 2013 Daniel Thorpe. All rights reserved.
+//
+
+#import "DNTUtilitiesTests.h"
+
+@implementation DNTUtilitiesTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in DNTUtilitiesTests");
+}
+
+@end
